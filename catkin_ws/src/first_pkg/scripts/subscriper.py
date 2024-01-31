b41fe541623142620c68542ffe6f1604b600dc54
Seif Allah <saifeldarageely@gmail.com>
2024-01-31 03:38:36 +0200

    #

--- catkin_ws/src/first_pkg/scripts/subscriper.py
+++ catkin_ws/src/first_pkg/scripts/subscriper.py
@@ -10,11 +10,6 @@ def listner():
  
     rospy.init_node('talker', anonymous=True)
     rospy.Subscriber('chatter', String, my_callack)
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        hello_str = "hello, world ! %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        rate.sleep()
 
 if __name__ == '__main__':
     try:
