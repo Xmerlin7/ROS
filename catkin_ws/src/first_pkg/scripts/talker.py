e379f9fb10adb6e42fc213f86984048fc86524ae
Seif Allah <saifeldarageely@gmail.com>
2024-01-31 02:34:32 +0200

    #

--- catkin_ws/src/first_pkg/scripts/talker.py
+++ catkin_ws/src/first_pkg/scripts/talker.py
@@ -7,8 +7,8 @@ def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10)
-    while not rospy.is_shutdown();
-        hello_str = "hello, world !" %s rospy.get_time()
+    while not rospy.is_shutdown():
+        hello_str = "hello, world ! %s" % rospy.get_time()
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
