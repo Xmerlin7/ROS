a8eaf8e1b4226378ce4bc54fdb1ed5ed54a71292
Seif Allah <saifeldarageely@gmail.com>
2024-01-31 03:35:47 +0200

    #

---
+++ catkin_ws/src/first_pkg/scripts/subscriper.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String, Int16
+
+def 
+
+def listner():
+ 
+    rospy.init_node('talker', anonymous=True)
+    rospy.Subscriber('chatter', String, my_callack)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        hello_str = "hello, world ! %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        listner()
+    except rospy.ROSInterruptException:
+        pass        
+
