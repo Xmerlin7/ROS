3e3b4b4a1ffd17f07300f4a50cc3f63ef7b2e2b1
Seif Allah <saifeldarageely@gmail.com>
2024-02-01 21:55:02 +0200

    #

--- catkin_ws/src/first_pkg/scripts/talker.py
+++ catkin_ws/src/first_pkg/scripts/talker.py
@@ -8,12 +8,16 @@ from first_pkg.msg import V2V
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
     car_pub = rospy.Publisher('car_chn', V2V, queue_size=10)
+    car_info = V2V()
+    car_info.name = "keya"
+    car_info.battery_level = .95
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         hello_str = "hello, world ! %s" % rospy.get_time()
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
+        pub.publish()
         rate.sleep()
 
 if __name__ == '__main__':
