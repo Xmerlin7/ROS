b46037f7b1e2ad9bb91deccdb946401e26419576
Seif Allah <saifeldarageely@gmail.com>
2024-02-01 23:23:35 +0200

    #

--- catkin_ws/src/image_viewer/scripts/image_viewer.py
+++ catkin_ws/src/image_viewer/scripts/image_viewer.py
@@ -15,6 +15,9 @@ def image_callback(ros_image):
         cv_image = bridge.imgmsg_to_cv2(ros_image, 'bgr8')
     except CvBridgeError as e:
         print(e)
+#   From now you can use opencv commnads    
+    cv2.imshow("image_viewer", cv_image)
+    cv2.waitKey(3)
 
 def main(args):
     rospy.init_node('image_viewer', anonymous=True)
