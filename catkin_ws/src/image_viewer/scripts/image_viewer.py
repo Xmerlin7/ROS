f5e141f6c98def9f762c8a7b57f0c83112ec694d
Seif Allah <saifeldarageely@gmail.com>
2024-02-01 23:12:06 +0200

    #

--- catkin_ws/src/image_viewer/scripts/image_viewer.py
+++ catkin_ws/src/image_viewer/scripts/image_viewer.py
@@ -7,4 +7,15 @@ from cv_bridge import CvBridge, CvBridgeError
 import sys
 bridge = CvBridge()
 
-def
+def main(args):
+    rospy.init_node('image_converter', anonymous=True)
+    image_sub = rospy.Subscriber("/usb_cam/image_raw", Image, image_callback)
+
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+        cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main(sys.argv)
+
