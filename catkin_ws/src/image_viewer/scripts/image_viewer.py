fd511137a524ea99f681c574c1d4045afa71a74d
Seif Allah <saifeldarageely@gmail.com>
2024-02-01 23:38:40 +0200

    #

--- catkin_ws/src/image_viewer/scripts/image_viewer.py
+++ catkin_ws/src/image_viewer/scripts/image_viewer.py
@@ -1,24 +1,21 @@
 import rospy
 import cv2
-
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import sys
-
 bridge = CvBridge()
 
 def image_callback(ros_image):
     print("got an image")
-    global bridge
 
     try:
         cv_image = bridge.imgmsg_to_cv2(ros_image, 'bgr8')
     except CvBridgeError as e:
         print(e)
-#   From now you can use opencv commnads    
+
     cv2.imshow("image_viewer", cv_image)
-    #cv2.waitKey(3)
+    cv2.waitKey(30)  # Adjust display duration as needed
 
 def main(args):
     rospy.init_node('image_viewer', anonymous=True)
@@ -29,6 +26,6 @@ def main(args):
     except KeyboardInterrupt:
         print("Shutting down")
         cv2.destroyAllWindows()
+
 if __name__ == '__main__':
     main(sys.argv)
-
